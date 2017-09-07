ec315a08a3550e510ae8964c8d2e7334e55fb268
Michael Nino Evensen <michaelevensen@gmail.com>
2017-09-07 11:23:06 +0200

    Initial Commit

---
+++ CustomTextFields/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTextFields
+//
+//  Created by Michael Evensen on 07/09/2017.
+//  Copyright © 2017 Michael Evensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
